5ca3eff71cd19e39b2ac57f5502788453e9acac3
Vyacheslav Krachkov <v.krachkov@enestech.net>
2020-11-16 09:32:37 +0200

    chg: base url for oauth2 provider

--- src/oauth2_server_project/urls.py
+++ src/oauth2_server_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path("oauth2/", include("oauth2_provider.urls", namespace="oauth2_provider")),
 ]
