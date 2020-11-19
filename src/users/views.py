e1d3c1aacad195cff775869b9477b38611f5dbf1
Vyacheslav Krachkov <v.krachkov@enestech.net>
2020-11-19 11:59:01 +0200

    chg: set `DjangoModelPermissions` as default permission class

--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,5 @@
 from typing import cast
 
-from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
 from oauth2_provider.models import AbstractAccessToken
 from rest_framework import serializers
 from rest_framework.exceptions import NotFound
@@ -16,15 +15,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class CurrentUserAPIView(RetrieveAPIView):
-    permission_classes = [TokenHasReadWriteScope]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def get_object(self) -> User:
-        token = self.request.auth
-        if hasattr(token, "scope"):  # OAuth 2
-            token = cast(AbstractAccessToken, token)
-            if user := token.user:
-                self.check_object_permissions(self.request, user)
-                return user
+        if user := self.request.user:
+            return user
         raise NotFound()
