0d5a50986ad91d32767d958365bd61bca2026249
Vyacheslav Krachkov <v.krachkov@enestech.net>
2020-12-13 00:24:30 +0200

    chg: alter path to sqlite db file

--- src/settings.py
+++ src/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": Path(__file__).resolve().parent.parent.parent / "db.sqlite3",
+        "NAME": Path(__file__).resolve().parent.parent / "db.sqlite3",
     }
 }
 
